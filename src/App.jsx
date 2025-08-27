3beaeac0d0de39678cb84fc3cd05a3518d12152b
TheJoeZone <jozsef.ujvari@outlook.com>
2025-08-27 14:26:53 +0200

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Anti-Scam Advisor Website</h1>
+      <p>This is your deployed React site on Vercel.</p>
+    </div>
+  );
+};
+
+export default App;
