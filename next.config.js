515a10c229e0ddce975b3d8e54a42071a5b93d34
TheJoeZone <jozsef.ujvari@outlook.com>
2025-08-27 13:19:17 +0200

    Delete next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ['cdn.abacus.ai', 'placehold.co']
-  }
-}
-
-module.exports = nextConfig
